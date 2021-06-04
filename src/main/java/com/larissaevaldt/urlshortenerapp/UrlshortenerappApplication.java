86565a70653ba4cff17a50bdff6f44a55be675ce
Larissa Evaldt <lari.justo@gmail.com>
2021-06-04 14:17:28 +0100

    first commit

---
+++ src/main/java/com/larissaevaldt/urlshortenerapp/UrlshortenerappApplication.java
@@ -0,0 +1,13 @@
+package com.larissaevaldt.urlshortenerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larissaevaldt/urlshortenerapp/UrlshortenerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissaevaldt.urlshortenerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
